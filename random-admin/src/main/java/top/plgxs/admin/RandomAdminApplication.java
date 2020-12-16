c7489b716a8b108dd228f60fb133b945fb0ba79e
lp1791803611 <lipian1004@163.com>
2020-12-16 22:20:16 +0800

    配置文件

---
+++ random-admin/src/main/java/top/plgxs/admin/RandomAdminApplication.java
@@ -0,0 +1,13 @@
+package top.plgxs.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomAdminApplication.class, args);
+	}
+
+}
---
+++ random-wx/src/test/java/top/plgxs/wx/RandomWxApplicationTests.java
@@ -0,0 +1,13 @@
+package top.plgxs.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomWxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
