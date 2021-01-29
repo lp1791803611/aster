0bacf338b67ee97b97d1cb0a9264f6929864070b
lp1791803611 <lipian1004@163.com>
2021-01-29 23:21:06 +0800

    修改bug

--- random-mbg/src/main/java/top/plgxs/mbg/mapper/sys/SysUserMapper.java
+++ random-mbg/src/main/java/top/plgxs/mbg/mapper/sys/SysUserMapper.java
@@ -1,10 +1,16 @@
 package top.plgxs.mbg.mapper.sys;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.data.repository.query.Param;
 import top.plgxs.mbg.entity.sys.SysUser;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -26,4 +32,7 @@ public interface SysUserMapper extends BaseMapper<SysUser> {
      * @date 2021-01-29
      */
     List<SysUser> selectSysUserList(SysUser sysUser);
+
+    IPage<Map<String,Object>> selectUserPage(Page<Map<String,Object>> page,
+                                             @Param(Constants.WRAPPER) QueryWrapper<Map<String,Object>> queryWrapper);
 }
