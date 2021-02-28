8ac85b862a50066a7f70d7ebcaef0cf1b8f7f64c
lp1791803611 <lipian1004@163.com>
2021-02-28 19:59:38 +0800

    集成redis

---
+++ random-common/random-common-redis/src/main/java/top/plgxs/common/redis/config/RedisConfig.java
@@ -0,0 +1,9 @@
+package top.plgxs.common.redis.config;
+
+/**
+ * @author Stranger。
+ * @version 1.0
+ * @since 2021/2/28 19:58
+ */
+public class RedisConfig {
+}
---
+++ random-common/random-common-redis/src/main/java/top/plgxs/common/redis/util/RedisUtils.java
@@ -0,0 +1,9 @@
+package top.plgxs.common.redis.util;
+
+/**
+ * @author Stranger。
+ * @version 1.0
+ * @since 2021/2/28 19:58
+ */
+public class RedisUtils {
+}
